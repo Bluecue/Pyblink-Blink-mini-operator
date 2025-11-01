075fd86913ee44c9aa2a0b6312e456707f1b6d7f
Bluecue <54121059+Bluecue@users.noreply.github.com>
2025-11-01 18:46:33 -0500

    Update Pyblinkstart.py
    Comments

--- Pyblinkstart.py
+++ Pyblinkstart.py
@@ -11,6 +11,6 @@ async def start():
     except BlinkTwoFARequiredError:
         await blink.prompt_2fa()
     return blink
-    try:
-        await blink.download_videos('/home/blink', since='2018/07/04 09:34', delay=2) 
+    try: ##added this, dont know how try or await works yet, but I know await belongs inside the async function
+        await blink.download_videos('/home/blink', since='2018/07/04 09:34', delay=2) ## needs to be a prompt for user input, asking what date range the user would like the downloaded video from.
  blink = asyncio.run(start())
