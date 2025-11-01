bc405244b4ac236a4fa8cac13e95c0cd959383d6
Bluecue <54121059+Bluecue@users.noreply.github.com>
2025-11-01 18:52:14 -0500

    Update Pyblinkstart.py
    Fixed try except thingy

--- Pyblinkstart.py
+++ Pyblinkstart.py
@@ -13,4 +13,5 @@ async def start():
     return blink
     try: ##added this, dont know how try or await works yet, but I know await belongs inside the async function
         await blink.download_videos('/home/blink', since='2018/07/04 09:34', delay=2) ## needs to be a prompt for user input, asking what date range the user would like the downloaded video from.
+    except print("An Exception occurred")
  blink = asyncio.run(start())
